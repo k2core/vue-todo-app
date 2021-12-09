0068ad42c777e46577e9b758057d1d55327bccde
k2core <7179062@gmail.com>
2021-12-09 23:41:42 +0900

    initial
    패캠 프로젝트 환경 구성 끝 개발 시작

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2015,
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: ['standard', 'plugin:vue/essential'],
+  plugins: ['vue'],
+  rules: {
+    'no-new': 0,
+    'space-before-function-paren': [
+      1,
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ]
+  }
+}
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true, // 문자열은 싱글쿼트
+  semi: false, // 코드 끝에 세미콜론
+  useTabs: false, // 들여쓰기 스케이스 사용
+  tabWidth: 2, // 들여쓰기 2칸
+  trailingComma: 'none', // 키:값 뒤에 항상 콤마
+  printWidth: 80, // 코드 한줄 최대 80
+  arrowParens: 'avoid', // 화살표 함수가 하나의 매개변수를 받을 때 괄호 생략
+  endOfLine: 'auto', // windows에 뜨는 'Delete cr' 에러 해결
+}
