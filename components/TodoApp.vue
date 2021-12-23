3cfc6629435cb731dcd54f34f4b1a9df2e7681c5
k2core <7179062@gmail.com>
2021-12-23 22:03:34 +0900

    7. CRUD - Create Todo(ERR 버전)
    컴파일은 오류가 없지만, 역시나 polyfill 문제로 실행 시에 브라우저에 오류가 나타난다.

--- components/TodoApp.vue
+++ components/TodoApp.vue
@@ -7,6 +7,7 @@
 
 <script>
 import { LowSync, LocalStorage } from 'lowdb'
+import cryptoRandomString from 'crypto-random-string'
 import TodoCreator from './TodoCreator'
 import TodoItem from './TodoItem'
 
@@ -32,6 +33,17 @@ export default {
       this.db.read()
       this.db.data ||= { todos: [] }
       this.db.write()
+    },
+    createTodo(title) {
+      const newTodo = {
+        id: cryptoRandomString({ length: 10 }),
+        title,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        done: false
+      }
+
+      this.db.todos.push(newTodo).write()
     }
   }
 }
