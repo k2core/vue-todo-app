26bb29b4e62bbb6e10e9d272a023b4b469e29a0d
k2core <7179062@gmail.com>
2022-01-09 19:22:38 +0900

    13. CRUD - Delete Todo

--- components/TodoApp.vue
+++ components/TodoApp.vue
@@ -19,6 +19,7 @@ import _cloneDeep from 'lodash/cloneDeep'
 // import _chain from 'lodash/chain'
 import _find from 'lodash/find'
 import _assign from 'lodash/assign'
+import _findIndex from 'lodash/findIndex'
 import TodoCreator from './TodoCreator'
 import TodoItem from './TodoItem'
 
@@ -97,8 +98,36 @@ export default {
       const foundTodo = _find(this.todos, { id: todo.id })
       _assign(foundTodo, value)
     },
-    deleteTodo() {
-      console.log('Delete todo!')
+    deleteTodo(todo) {
+      /**
+       * 강의 버전(lowdb 1.0.0)
+       */
+      // this.db
+      //   .get('todos')
+      //   .remove({ id: todo.id })
+      //   .write()
+
+      /**
+       * 단순 for문
+       */
+      // for (let i = 0; i < this.db.data.todos.length; i++) {
+      //   if (this.db.data.todos[i].id === todo.id) {
+      //     this.db.data.todos.splice(i, 1)
+      //     this.db.write()
+      //     break
+      //   }
+      // }
+      /**
+       * 아래 this.todos에서 사용한 Lodash.findIndex 사용하기.
+       * db와 화면이 동기화 되어 있어서 index를 한 번만 찾으면 되겠지만,
+       * 혹시 몰라서 각각 찾아서 처리하는 것으로 했다.
+       */
+      const foundIndex1 = _findIndex(this.db.data.todos, { id: todo.id })
+      this.db.data.todos.splice(foundIndex1, 1)
+      this.db.write()
+
+      const foundIndex2 = _findIndex(this.todos, { id: todo.id })
+      this.$delete(this.todos, foundIndex2)
     }
   }
 }
